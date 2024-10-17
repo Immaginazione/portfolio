048ce681cb35f6834c3939199a03047bd7519a1a
Immaginazione <135206039+Immaginazione@users.noreply.github.com>
2024-10-17 18:04:55 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
 from flask import Flask, render_template, url_for
+from sqlalchemy import create_engine,text
+import random
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("home.html")
+
+    engine = create_engine("mysql+pymysql://root@127.0.0.1/corsosql?charset=utf8mb4")
+    with engine.connect() as conn:
+        sql = text("SELECT cust_name, cust_email from customers")
+        result = conn.execute(sql)
+        clienti = result.all()
+
+    rng = random.choice(["beppe","lorenzo", "mattia", "luca","ernesto"])
+    return render_template("home.html", nome=rng, clienti=clienti)
 
 @app.route("/about-me")
 def about():
