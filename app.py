97d25a66d1e692d5f6034b74a7cccd7d05dd80e8
Immaginazione <135206039+Immaginazione@users.noreply.github.com>
2024-10-17 16:53:16 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -1,7 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
     return render_template("home.html")
+
+@app.route("/about-me")
+def about():
+    return render_template("about-me.html")
+
+@app.route("/contacts")
+def contacts():
+    return render_template("contacts.html")
